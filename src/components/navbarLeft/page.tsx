f6e66c648c2d606e06a96e3725a62ffca0ec2648
diegoandres22 <diego.a.v3005@gmail.com>
2024-06-25 13:49:47 -0400

    FIX: cambio idioma de direccion de productos, instalo axios, añado variables de endpoints a usar

--- src/components/navbarLeft/page.tsx
+++ src/components/navbarLeft/page.tsx
@@ -18,7 +18,7 @@ export default function NavBar() {
   return (
     <div className='flex w-[20vw] h-screen bg-white bg-opacity-10 flex-col p-5 pt-20 gap-5'>
       <Link
-        href="/inicio"
+        href="/start"
         onClick={() => dispatch(setTitle(Titles.First))}
         className={getLinkClasses(Titles.First)}
       >
@@ -68,7 +68,7 @@ export default function NavBar() {
       </Accordion>
 
       <Link
-        href="/compras"
+        href="/purchases"
         onClick={() => dispatch(setTitle(Titles.Third))}
         className={getLinkClasses(Titles.Third)}
       >
@@ -76,7 +76,7 @@ export default function NavBar() {
       </Link>
 
       <Link
-        href="/metricas"
+        href="/metrics"
         onClick={() => dispatch(setTitle(Titles.Fourth))}
         className={getLinkClasses(Titles.Fourth)}
       >
@@ -84,7 +84,7 @@ export default function NavBar() {
       </Link>
 
       <Link
-        href="/productos"
+        href="/products"
         onClick={() => dispatch(setTitle(Titles.Fifth))}
         className={getLinkClasses(Titles.Fifth)}
       >
@@ -92,7 +92,7 @@ export default function NavBar() {
       </Link>
 
       <Link
-        href="/clientes"
+        href="/customers"
         onClick={() => dispatch(setTitle(Titles.Sixth))}
         className={getLinkClasses(Titles.Sixth)}
       >
@@ -100,7 +100,7 @@ export default function NavBar() {
       </Link>
 
       <Link
-        href="/promociones"
+        href="/promotions"
         onClick={() => dispatch(setTitle(Titles.Seventh))}
         className={getLinkClasses(Titles.Seventh)}
       >
@@ -108,7 +108,7 @@ export default function NavBar() {
       </Link>
 
       <Link
-        href="/libro_rojo"
+        href="/red_book"
         onClick={() => dispatch(setTitle(Titles.Eighth))}
         className={getLinkClasses(Titles.Eighth)}
       >
@@ -116,7 +116,7 @@ export default function NavBar() {
       </Link>
 
       <Link
-        href="/auditorias"
+        href="/audits"
         onClick={() => dispatch(setTitle(Titles.Nineth))}
         className={getLinkClasses(Titles.Nineth)}
       >
@@ -126,86 +126,3 @@ export default function NavBar() {
   );
 }
 
-
-
-
-// 'use client'
-
-// import React from 'react'
-// import { Titles } from '../../variables';
-// import Link from 'next/link';
-// import { Accordion, AccordionItem, AccordionButton, AccordionIcon, AccordionPanel, Box } from '@chakra-ui/react';
-// import { useAppDispatch } from '@/redux/services/hooks';
-// import { setTitle } from "@/redux/slices/titleSlice"
-// import { RootState } from '@/redux/store';
-// import { useSelector } from 'react-redux';
-
-// export default function NavBar() {
-
-//   const title = useSelector((state: RootState) => state.titles)
-//   const dispatch = useAppDispatch();
-
-//   return (
-//     <div className='flex w-[20vw] h-screen bg-white bg-opacity-10 flex-col p-5 pt-20 gap-5'>
-
-//       {title.title == Titles.First ? <Link href="/inicio" onClick={() => dispatch(setTitle(Titles.First))} className='text-xl font-bold text-buttomClick' >{Titles.First} </Link> : <Link href="/inicio" onClick={() => dispatch(setTitle(Titles.First))}>{Titles.First}</Link>
-//       }
-
-//       <Accordion allowMultiple>
-//         <AccordionItem>
-
-//           <AccordionButton>
-//             <Box as='span' flex='1' textAlign='left'>
-//               Ventas
-//             </Box>
-//             <AccordionIcon />
-//           </AccordionButton>
-
-//           <AccordionPanel className=''>
-//             <ul className='flex flex-col gap-4 mt-4 ml-10'>
-
-//               {title.title == Titles.Sales1 ? <li>
-//                 <Link href="/ventas/resumen" onClick={() => dispatch(setTitle(Titles.Sales1))} className='text-xl font-bold text-buttomClick'>
-//                   {Titles.Sales1}
-//                 </Link>
-//               </li> : <li>
-//                 <Link href="/ventas/resumen" onClick={() => dispatch(setTitle(Titles.Sales1))} >
-//                   {Titles.Sales1}
-//                 </Link>
-//               </li>}
-
-
-//               {title.title == Titles.Sales1}
-//               <li><Link href="/ventas/actual" onClick={() => dispatch(setTitle(Titles.Sales2))}>{Titles.Sales2}</Link></li>
-
-//               {title.title == Titles.Sales1}
-//               <li><Link href="/ventas/metodos" onClick={() => dispatch(setTitle(Titles.Sales3))}>{Titles.Sales3}</Link></li>
-//             </ul>
-//           </AccordionPanel>
-//         </AccordionItem>
-//       </Accordion>
-
-//       {title.title == Titles.Sales1}
-//       <Link href="/compras" onClick={() => dispatch(setTitle(Titles.Third))}>{Titles.Third}</Link>
-
-//       {title.title == Titles.Sales1}
-//       <Link href="/metricas" onClick={() => dispatch(setTitle(Titles.Fourth))}>{Titles.Fourth}</Link>
-
-//       {title.title == Titles.Sales1}
-//       <Link href="/productos" onClick={() => dispatch(setTitle(Titles.Fifth))}>{Titles.Fifth}</Link>
-
-//       {title.title == Titles.Sales1}
-//       <Link href="/clientes" onClick={() => dispatch(setTitle(Titles.Sixth))}>{Titles.Sixth}</Link>
-
-//       {title.title == Titles.Sales1}
-//       <Link href="/promociones" onClick={() => dispatch(setTitle(Titles.Seventh))}>{Titles.Seventh}</Link>
-
-//       {title.title == Titles.Sales1}
-//       <Link href="libro_rojo" onClick={() => dispatch(setTitle(Titles.Eighth))}>{Titles.Eighth}</Link>
-
-//       {title.title == Titles.Sales1}
-//       <Link href="/auditorias" onClick={() => dispatch(setTitle(Titles.Nineth))}>{Titles.Nineth}</Link>
-
-//     </div>
-//   )
-// }
--- src/variables.ts
+++ src/variables.ts
@@ -18,3 +18,19 @@ export const Titles = {
     Eighth: "Libro Rojo",
     Nineth: "Auditorias"
 };
+
+//Variables Endspoints para consumo de api 
+
+export const API_BASE_URL_ventas = 'http://26.96.128.174:9090/xconnect/api/ExtractionData';
+export const API_BASE_URL_compras = 'http://26.96.128.174:9090/xconnect/api/ExtractionData';
+export const API_BASE_URL_pagos = 'http://26.96.128.174:9090/xconnect/api/ExtractionData';
+
+
+export const endpoints = {
+    salesFull: (dateFrom: string, dateEnd: string) =>
+        `${API_BASE_URL_ventas}/SalesFull?dateFrom=${dateFrom}&dateEnd=${dateEnd}`,
+    purchaseFull: (dateFrom: string, dateEnd: string) =>
+        `${API_BASE_URL_compras}/PurchaseFull?dateFrom=${dateFrom}&dateEnd=${dateEnd}`,
+    SalesPayFull: (dateFrom: string, dateEnd: string) =>
+        `${API_BASE_URL_compras}/SalesPay?dateFrom=${dateFrom}&dateEnd=${dateEnd}`,
+};
